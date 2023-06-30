1e5463cd5f783923a068a5f85bb61b1ec5b7a9ec
dam900 <damianjakubowski48@gmail.com>
2023-06-30 17:56:31 +0200

    deleted unused files

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
 import cv2 as cv
 import sys
 
-import numpy as np
-
 from canny_edge_detector import CannyEdgeDetector
-from processors import ImageProcessor
-from processors import ProcessorSettings as Ps
 
 
 def main():
     img = cv.imread(r'blok.jpg')
-    i = ImageProcessor(img)
     if img is None:
         sys.exit("Could not read the image.")
 
--- processors/__init__.py
+++
@@ -1,4 +0,0 @@
-from .video_processor import VideoProcessor
-from .abstract_processor import Processor
-from .image_processor import ImageProcessor
-from .processor_settings import ProcessorSettings
