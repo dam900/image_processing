3221454a31fcade5d130c995de67cc7dde310f8e
dam900 <damianjakubowski48@gmail.com>
2023-07-02 18:27:39 +0200

    line detecting algorithm fixed

--- line_detector.py
+++ line_detector.py
@@ -1,19 +1,37 @@
 import numpy as np
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return (f"Point({self.x} ,{self.y})")
+
+
+class Path:
+    def __init__(self, start: Point, stop: Point):
+        self.start = start
+        self.stop = stop
+
+    def __repr__(self):
+        return f"start {self.start} ,stop {self.stop}"
+
+
 class LineDetector:
 
     @classmethod
-    def hough_line(cls, src: np.ndarray):
-        y, x = src.shape
+    def hough_line(cls, src: np.ndarray, threshold: int = 100):
+        Ny, Nx = src.shape
         # max dist from origin in polar coordinates
-        max_r = int(np.round(np.sqrt(y ** 2) + np.sqrt(x ** 2)))
+        max_r = int(np.round(np.sqrt(Ny ** 2) + np.sqrt(Nx ** 2)))
         # angles in polar coordinates
         thetas = np.deg2rad(np.arange(-90, 90))
         # radius range
         rs = np.linspace(-max_r, max_r, 2 * max_r)
         # array for the accumulation of crossings in polar coordinates
-        acc = np.zeros((2 * max_r, len(rs)))
+        acc = np.zeros((2 * max_r, len(thetas)))
 
         with np.nditer(src, flags=['multi_index'], op_flags=['readwrite']) as it:
             for x in it:
@@ -21,8 +39,22 @@ class LineDetector:
                     with np.nditer(thetas, flags=['f_index']) as thetas_it:
                         for k in thetas_it:
                             i, j = it.multi_index
-                            r = i*np.cos(k) + j*np.sin(k)
+                            r = j * np.cos(k) + i * np.sin(k)
                             acc[int(r) + max_r, thetas_it.index] += 1
-        return acc, thetas, rs
+        lines = np.argwhere(acc > threshold)
+        dest_theta, dest_rs = [], []
+        for y, x in lines:
+            dest_theta.append(np.rad2deg(thetas[x]))
+            dest_rs.append(int(rs[y]))
+        return acc, dest_theta, dest_rs
 
-
+    @classmethod
+    def get_points(cls, thetas: list, rs: list):
+        paths: [Path] = []
+        for theta, rho in zip(thetas, rs):
+            sin, cos = np.sin(theta), np.cos(theta)
+            x0, y0 = cos * rho, sin * rho
+            pt1 = Point(int(x0 + 1000 * (-sin)), int(y0 + 1000 * cos))
+            pt2 = Point(int(x0 - 1000 * (-sin)), int(y0 - 1000 * cos))
+            paths.append(Path(pt1, pt2))
+        return paths
--- main.py
+++ main.py
@@ -3,18 +3,31 @@ import numpy as np
 import sys
 
 from canny_edge_detector import CannyEdgeDetector
+from line_detector import LineDetector
 
 
 def main():
-    img = cv.imread(r'blok.jpg')
-    if img is None:
-        sys.exit("Could not read the image.")
+    # img = cv.imread(r'blok.jpg')
+    # if img is None:
+    #     sys.exit("Could not read the image.")
 
     # canny = CannyEdgeDetector.Canny(img)
+    # canny = cv.Canny(img, 150, 240)
+    img = np.zeros((200, 200))
+    img[...] = np.flip(np.eye(200), axis=1) + np.eye(200)
+    bgr = np.zeros((200, 200, 3))
+    bgr[..., 1] = img
+    acc, theta, rs = LineDetector.hough_line(img, threshold=150)
+    lines = LineDetector.get_points(theta, rs)
+    # cdst = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
+    for line in lines:
+        p1 = line.start
+        p2 = line.stop
+        cv.line(bgr, (p1.x, p1.y), (p2.x, p2.y), (0, 0, 255), 1, cv.LINE_AA)
 
-
-    canny = cv.Canny(img, threshold1=150, threshold2=200)
-    lines = cv.HoughLines(canny, 1, np.pi / 180, 400,  None, 0, 0)
+    # canny = cv.Canny(img, threshold1=150, threshold2=240)
+    # cdst = cv.cvtColor(canny, cv.COLOR_GRAY2BGR)
+    # lines = cv.HoughLines(canny, 1, np.pi / 180, 150,  None, 0, 0)
     # if lines is not None:
     #     for i in range(0, len(lines)):
     #         rho = lines[i][0][0]
@@ -25,9 +38,9 @@ def main():
     #         y0 = b * rho
     #         pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
     #         pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
-    #         cv.line(img, pt1, pt2, (0, 0, 255), 1, cv.LINE_AA)
+    #         cv.line(cdst, pt1, pt2, (0, 0, 255), 1, cv.LINE_AA)
 
-    cv.imshow("Display window", img)
+    cv.imshow("Display window", bgr)
     key = cv.waitKey(0)
     if key == ord("q"):
         cv.destroyAllWindows()
